cf77fa3a985f31ca1fc01b212e9c724abcc8a4ca
yilongdong <dyl20001223@163.com>
2023-02-13 15:25:38 +0800

    build(sanitizers): debug模式下增加内存泄露，未定义行为的检查

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 # shellcheck disable=SC2046
 # shellcheck disable=SC2215
-cmake -B build/debug -S . -G Ninja -DCMAKE_BUILD_TYPE=Debug \                                                                                           [23:55:14]
-  -DCMAKE_TOOLCHAIN_FILE=$(dirname $(which vcpkg))/scripts/buildsystems/vcpkg.cmake \
-  -DCMAKE_INSTALL_PREFIX=$(pwd)/install
+cmake -B build/debug -S . -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=$(dirname $(which vcpkg))/scripts/buildsystems/vcpkg.cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/install
 cmake --build build/debug --parallel
--- standalone/main.cpp
+++ standalone/main.cpp
@@ -1,7 +1,11 @@
 #include "foo.h"
 #include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
     Hello();
-    return 0;
+    char *x = (char*)malloc(10 * sizeof(char*));
+    free(x);
+    int *array = new int[100];
+    delete [] array;
+    return array[argc];  // BOOM
 }
