d04a2fde4dd28e3c1388fc8a48cd0b7ff5e19f2b
yilongdong <dyl20001223@163.com>
2023-02-13 16:26:04 +0800

    perf(add gperftool): 由于gperftools的issue#1292，目前无法进行性能测试，以后用到了再尝试修复

--- standalone/main.cpp
+++ standalone/main.cpp
@@ -5,7 +5,5 @@ int main(int argc, char* argv[]) {
     Hello();
     char *x = (char*)malloc(10 * sizeof(char*));
     free(x);
-    int *array = new int[100];
-    delete [] array;
-    return array[argc];  // BOOM
+    return 0;
 }
